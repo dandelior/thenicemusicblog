55dc8bae6dba48f0490c00dc4f2e1571e676404b
Juan Berrios <hola@juanberrios.com>
2021-11-03 10:27:52 -0300

    ItemGrid Component added

---
+++ components/ItemGrid.js
@@ -0,0 +1,14 @@
+const ItemGrid = () => {
+   return (
+      <div className="item">
+         <div className="img">
+         </div>
+         <div className="data">
+            <h3>Título del album</h3>
+            <p></p>
+         </div>
+      </div>
+   )
+}
+
+export default ItemGrid;
